c6ce7482a07d77c856cfb9a1dcf1121156b35092
Siggy <sigangawawdo@gmail.com>
2026-03-06 17:54:56 -0800

    Initial Commit

---
+++ Listycal/ListycalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListycalApp.swift
+//  Listycal
+//
+//  Created by Siggy Wawdo on 3/6/26.
+//
+
+import SwiftUI
+
+@main
+struct ListycalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
